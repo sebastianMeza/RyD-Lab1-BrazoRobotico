989d21c10ffdd4e8fb0716787026d66638fa7a43
sebastianMeza <sebastian.meza@usach.cl>
2016-05-25 11:14:18 -0400

    update 1.0

--- server.js
+++ server.js
@@ -1,3 +1,92 @@
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
 
 /*
 //sensor 1: sensor de luz
@@ -88,10 +177,15 @@ board.on("ready", function() {
     pin: 5,
     type: "continuous"    
   });
+  var piezo = new five.Piezo(10);
   //base inicial=90, 0 y 180
   //art1 inicial= ,atras=60, adelante=;
   //pinza inicial= 180, 90= cerrado, 180= abierto
 	
+  board.repl.inject({
+    piezo: piezo
+  });
+
   servo_base.to(85);
 	servo_1.to(30);
 	servo_2.to(145);
@@ -136,6 +230,13 @@ board.on("ready", function() {
     //responder al html
     res.status(200).end();
   
+    piezo.play({
+      song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -",
+      beats: 1 / 4,
+      tempo: 100
+    });   
+
+
     //VALORES INICIALES  
     servo_base.to(85);
     servo_1.to(30);
@@ -143,11 +244,6 @@ board.on("ready", function() {
     servo_pinza.to(0);
 
 
-/*    setTimeout(function(){
-      mover_pos2_piso3();
-      console.log("mover pos1");
-    }, 1000);
-*/
 
     setTimeout(function(){
       mover_pos1_piso3();
@@ -283,8 +379,63 @@ board.on("ready", function() {
       console.log("mover volver 9");
     }, 129*1000);
 
+    setTimeout(function(){
+      mover_pos3_piso3();
+      console.log("mover pos1");
+    }, 132*1000);
+    setTimeout(function(){
+      mover_volver();
+      console.log("mover volver 10");
+    }, 140*1000);
+
+    setTimeout(function(){
+      mover_winner();
+      console.log("mover winner");
+    }, 143*1000);
+
 
 });
+
+
+function mover_winner(){
+  setTimeout(function(){
+    servo_pinza.to(0);
+  }, 500);
+
+  setTimeout(function(){
+    servo_pinza.to(90);
+  }, 2*500);
+
+  setTimeout(function(){
+    servo_pinza.to(0);
+  }, 3*500);
+
+}
+
+
+function mover_pos3_piso3(){
+  setTimeout(function(){
+    //servo_pinza.to(90);
+    servo_base.to(60);
+  }, 1000);
+
+  //con 'i' desde 30 a 115 en intervalos de 1 segundo
+  timeout([30, 95], 0.1, function(i){
+    servo_1.to(i);
+    //console.log("servo_1: "+i);
+  });
+  timeout([120, 145], 0.1, function(i){
+    servo_2.to(120+145-i);
+    //console.log("servo_2: "+(115+145-i));
+  });
+  //pinza cerrar
+  setTimeout(function(){
+    timeout_pinza([0, 90], 1, function(i){
+      servo_pinza.to(i);
+      //console.log("pinza: "+(90-i));
+    });
+  }, 15*100);
+}
 
 
 function mover_pos1_piso3(){
